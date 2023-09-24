704d96fc417e9c3d99cd039c80c9e627af2ece3c
Andrew Ross <andrew.ross4224@gmail.com>
2023-09-24 15:30:18 -0400

    finished code for local storage, added clear button to head of page

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,15 +1,13 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
   // Saves block data into local storage on button click
+  var container = $(".container-fluid");
+  var clear = $("#clear")
+  clear.on('click',clearStorage);
   $(".block").on('click', save);
-  var clicked;
-  var text;
   function save(event) {
     if (event.target.tagName.toLowerCase() === 'button') {
-      clicked = $(event.target.parentElement);
-      text = clicked.children('textarea').val();
+      var clicked = $(event.target.parentElement);
+      var text = clicked.children('textarea').val();
       localStorage.setItem(event.target.parentElement.id, text);
     }
   }
@@ -20,16 +18,11 @@ $(function () {
     var currentDate = $("#date")
     currentDate.text(date)
   };
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+  // Code for setting color of text background based on hour
   setInterval(updateColor, 300000)
   function updateColor() {
-    var container = $(".container-fluid");
     var hour = dayjs().format('H')
-    
+
     container.children().each(function (i) {
       var test = container.children().eq(i).attr('id');
       if (hour === test) {
@@ -41,13 +34,24 @@ $(function () {
       }
     })
   }
-  updateColor();
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  // function to set textarea values to those saved in local storage
+  function setStorage() {
+    container.children().each(function (i) {
+      var index = container.children().eq(i).attr('id');
+      var stored = localStorage.getItem(index)
+      container.children().eq(i).children('textarea').text(stored)
+    })
+  }
+  // function for clearing local storage
+  function clearStorage(){
+    localStorage.clear();
+    location.reload()
+  }
+  // function to initialize page on load
+  function init() {
+    updateColor();
+    setStorage();
+  }
+  init();
 });
-
 
