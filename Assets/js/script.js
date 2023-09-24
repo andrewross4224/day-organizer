8cacecdc4157d2c2b866a63be55df71879dca22d
Andrew Ross <andrew.ross4224@gmail.com>
2023-09-24 13:41:47 -0400

    Updated HTML structure for hours, added color coding for hours based on current time, added clock in header for date and time

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,18 +2,46 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
+  // Saves block data into local storage on button click
+  $(".block").on('click', save);
+  var clicked;
+  var text;
+  function save(event) {
+    if (event.target.tagName.toLowerCase() === 'button') {
+      clicked = $(event.target.parentElement);
+      text = clicked.children('textarea').val();
+      localStorage.setItem(event.target.parentElement.id, text);
+    }
+  }
+  // Code for the clock at the top of the page
+  setInterval(updateTime, 1000)
+  function updateTime() {
+    var date = dayjs().format("[The current date is] MMM DD, YYYY [at] hh:mm:ss a")
+    var currentDate = $("#date")
+    currentDate.text(date)
+  };
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
+  setInterval(updateColor, 300000)
+  function updateColor() {
+    var container = $(".container-fluid");
+    var hour = dayjs().format('H')
+    
+    container.children().each(function (i) {
+      var test = container.children().eq(i).attr('id');
+      if (hour === test) {
+        container.children().eq(i).children('textarea').addClass('present');
+      } else if (hour > test) {
+        container.children().eq(i).children('textarea').addClass('past');
+      } else {
+        container.children().eq(i).children('textarea').addClass('future');
+      }
+    })
+  }
+  updateColor();
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
@@ -21,3 +49,5 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+
+
